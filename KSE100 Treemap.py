fcbdef7f890293cbc91c6536c6091d2cc3745f11
AbdurR67 <163110587+AbdurR67@users.noreply.github.com>
2024-04-15 20:04:54 +0500

    Update KSE100 Treemap.py

--- KSE100 Treemap.py
+++ KSE100 Treemap.py
@@ -7,14 +7,14 @@ username = os.getlogin()
 
 sectors_df = pd.read_excel(fr'C:\Users\{username}\Dropbox\AR\Sectors_KSE100.xlsx') #excel file location
 
-url = "https://dps.psx.com.pk/indices/KSE100"
+url = 'https://dps.psx.com.pk/indices/KSE100'
 data = pd.read_html(url)[0]
 data.columns = ['SYMBOL', 'NAME', 'LDCP', 'CURRENT', 'CHANGE', 'CHANGE (%)', 'IDX WTG (%)', 'IDX POINT', 'VOLUME', 'FREEFLOAT (M)', 'MARKET CAP (M)']
 data['SYMBOL'] = data['SYMBOL'].str.replace('XD', '')
 data['Change (%)'] = data['CHANGE (%)'].str.rstrip('%').astype(float)
-data['CHANGE (%)2'] = data['CHANGE (%)']
+data['CHANGE (%)2'] = data['CHANGE (%)'] #Just to display with % sign
 
-data = data[data['VOLUME'] != 0]
+data = data[data['VOLUME'] != 0] #in case want to make box size based on volume
 
 sectors_df['Symbol'] = sectors_df['Symbol'].str.split(', ')
 sectors_df = sectors_df.explode('Symbol')
@@ -23,7 +23,7 @@ merged_data = pd.merge(data, sectors_df, left_on='SYMBOL', right_on='Symbol', ho
 
 fig = px.treemap(merged_data, path=['Sector', 'SYMBOL'], values='MARKET CAP (M)',
                  color='Change (%)', hover_data=['NAME', 'MARKET CAP (M)', 'CHANGE (%)2'],
-                 color_continuous_scale=['red', "#414554", 'green'], color_continuous_midpoint=0,
+                 color_continuous_scale=['red', '#414554', 'green'], color_continuous_midpoint=0,
                  range_color=[-3, 3])
 
 
@@ -32,12 +32,12 @@ fig.update_coloraxes(showscale=False)
 
 # To show Values also
 fig.update_traces(texttemplate='%{label}<br>%{customdata[2]}')
-fig.update_traces(textposition="middle center", selector=dict(type='treemap'))
+fig.update_traces(textposition='middle center', selector=dict(type='treemap'))
 
 fig.update_traces(root_color='#242c34', selector=dict(type='treemap'))
 
 fig.update_layout(
-    title="KSE100 - Powered by mettisglobal.news",
+    title='KSE100 - Powered by mettisglobal.news',
     title_font_size=12,
     title_x=0.5,
     title_y=0.987,
@@ -50,5 +50,5 @@ fig.update_layout(
 
 #fig.update_coloraxes(colorbar_orientation='h', colorbar_len=0.5, colorbar_thickness=20)
 
-fig.write_image("KSE100_Map.png", width=960, height=640, scale=3)
+fig.write_image('KSE100_Map.png', width=960, height=640, scale=3)
 #fig.show()  #can enable this to view interatctive chart on browser
