cc8e5c09e9b971aab9178b4b1fdcb77277043a64
AbdurR67 <163110587+AbdurR67@users.noreply.github.com>
2024-05-24 12:41:04 +0500

    Update KSE100 Treemap.py

--- KSE100 Treemap.py
+++ KSE100 Treemap.py
@@ -5,32 +5,34 @@ from datetime import datetime
 import plotly.express as px
 username = os.getlogin()
 
-sectors_df = pd.read_excel(fr'C:\Users\{username}\Dropbox\AR\Sectors_KSE100.xlsx') #excel file location
+sectors_df = pd.read_excel(fr'C:\Users\{username}\Dropbox\AR\Sectors_KSE100.xlsx')
 
 url = 'https://dps.psx.com.pk/indices/KSE100'
 data = pd.read_html(url)[0]
 data.columns = ['SYMBOL', 'NAME', 'LDCP', 'CURRENT', 'CHANGE', 'CHANGE (%)', 'IDX WTG (%)', 'IDX POINT', 'VOLUME', 'FREEFLOAT (M)', 'MARKET CAP (M)']
 data['SYMBOL'] = data['SYMBOL'].str.replace(r'(XD|XB|XR)', '', regex=True)
 data['Change (%)'] = data['CHANGE (%)'].str.rstrip('%').astype(float)
-data['CHANGE (%)2'] = data['CHANGE (%)'] #Just to display with % sign
+data['CHANGE (%)2'] = data['CHANGE (%)']
 
-data = data[data['VOLUME'] != 0] #in case want to make box size based on volume
+#data = data[data['VOLUME'] != 0] # In case want to sort by volume
 
 sectors_df['Symbol'] = sectors_df['Symbol'].str.split(', ')
 sectors_df = sectors_df.explode('Symbol')
 merged_data = pd.merge(data, sectors_df, left_on='SYMBOL', right_on='Symbol', how='left')
 
 
+# Treemap Plot
 fig = px.treemap(merged_data, path=['Sector', 'SYMBOL'], values='MARKET CAP (M)',
                  color='Change (%)', hover_data=['NAME', 'MARKET CAP (M)', 'CHANGE (%)2'],
-                 color_continuous_scale=['red', '#414554', 'green'], color_continuous_midpoint=0,
+                 color_continuous_scale=['red', '#414554', '#30CC5A'], color_continuous_midpoint=0,
                  range_color=[-3, 3])
+            
 
 
 fig.update_traces(marker_line_color='#242c34', marker_line_width=1.5)
-fig.update_coloraxes(showscale=False)
 
-# To show Values also
+fig.update_coloraxes(showscale=False)
+# Update hover
 fig.update_traces(texttemplate='%{label}<br>%{customdata[2]}')
 fig.update_traces(textposition='middle center', selector=dict(type='treemap'))
 
@@ -50,5 +52,10 @@ fig.update_layout(
 
 #fig.update_coloraxes(colorbar_orientation='h', colorbar_len=0.5, colorbar_thickness=20)
 
-fig.write_image('KSE100_Map.png', width=960, height=640, scale=3)
-#fig.show()  #can enable this to view interatctive chart on browser
+today = datetime.today().strftime('%Y-%m-%d')
+download_dir = os.path.expanduser('~' + os.path.sep + 'Downloads')
+file_name = f'KSE100_Map_{today}.png'
+fig.write_image(os.path.join(download_dir, file_name), width=960, height=640, scale=3)
+
+
+#fig.show()  
